51165a95bb5cdc6736397434444de5decebecd81
Fabien Naour <fabiennaour@gmail.com>
2018-10-31 19:16:59 +0100

    change display friend

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -3,7 +3,6 @@ import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 //import "flatpickr/dist/themes/airbnb.css" // A path to the theme CSS
 
 flatpickr(".datepicker", {
-      minDate: "today",
 })
 
 
