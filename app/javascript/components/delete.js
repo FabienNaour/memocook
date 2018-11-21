ab92c113aed20133a5e98a9cddb7a0f20fd3af46
Fabien Naour <fabiennaour@gmail.com>
2018-11-22 00:02:07 +0100

    update display recipe and friends

--- app/javascript/components/delete.js
+++ app/javascript/components/delete.js
@@ -1,2 +1,30 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   //get moadals by class name
 
+//   const modals = document.querySelectorAll(".iframemodal");
+
+//   //if no modals, return
+//   if(!modals.length)
+//     return;
+
+
+
+//   //for each modal, we will listen for a click and bind click with named function
+//     modals.forEach((link) => {
+//       link.addEventListener("click", bindModalClick);
+//     });
+
+//   });
+
+
+
+//   const bindModalClick = (event) => {
+
+//      console.log("DELETE")
+
+
+
+//      //open modal with JQuery
+//      $('#deleteModal').modal();
+// }
 
