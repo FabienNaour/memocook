59a2248aae24007b648d6d121b7e95d7511bc3e6
Fabien Naour <fabiennaour@gmail.com>
2018-11-23 00:51:11 +0100

    modif boutons add

--- app/javascript/components/reception.js
+++ app/javascript/components/reception.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
-  $(".category-choice").click(function(){
-    $(this).toggleClass("active");
-  });
-});
+// $(document).ready(function(){
+//   $(".category-choice").click(function(){
+//     $(this).toggleClass("active");
+//   });
+// });
 
 
-$(document).ready(function(){
-  $(".category-choice0").click(function(){
-    $(this).toggleClass("active");
-  });
-});
+// $(document).ready(function(){
+//   $(".category-choice0").click(function(){
+//     $(this).toggleClass("active");
+//   });
+// });
 
-$(document).ready(function(){
-  $(".category-choice1").click(function(){
-    $(this).toggleClass("active");
-  });
-});
+// $(document).ready(function(){
+//   $(".category-choice1").click(function(){
+//     $(this).toggleClass("active");
+//   });
+// });
